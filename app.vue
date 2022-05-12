d7c38cb3d2264e7723e613fc984a1f6cf1771d68
ilg2005 <igoldfarb10@gmail.com>
2022-05-12 19:56:26 +0300

    initial commit: vuetify + card

--- app.vue
+++ app.vue
@@ -1,27 +1,35 @@
 <template>
-  <div>
-      <v-card
-          class="mx-auto"
-          max-width="344"
-          variant="outlined"
-      >
-        <v-card-header>
-          <div>
-            <div class="text-overline mb-1">
-              OVERLINE
-            </div>
-            <div class="text-h6 mb-1">
-              Headline
-            </div>
-            <div class="text-caption">Greyhound divisely hello coldly fonwderfully</div>
-          </div>
-        </v-card-header>
+  <v-card
+      class="mx-auto"
+      max-width="400"
+  >
+    <v-img
+        class="align-end text-white"
+        height="200"
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        cover
+    >
+      <v-card-title>Top 10 Australian beaches</v-card-title>
+    </v-img>
 
-        <v-card-actions>
-          <v-btn variant="outlined">
-            Button
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-  </div>
+    <v-card-subtitle class="pt-4">
+      Number 10
+    </v-card-subtitle>
+
+    <v-card-text>
+      <div>Whitehaven Beach</div>
+
+      <div>Whitsunday Island, Whitsunday Islands</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="orange">
+        Share
+      </v-btn>
+
+      <v-btn color="orange">
+        Explore
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
