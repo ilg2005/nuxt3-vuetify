91c981b98b5c86d4adf77e29e38ad6a731c67c07
ilg2005 <igoldfarb10@gmail.com>
2022-05-13 20:11:09 +0300

    AuthForm

--- components/AuthForm.vue
+++ components/AuthForm.vue
@@ -3,17 +3,64 @@
       class="mx-auto form"
       max-width="500"
   >
-    <v-card-title class="text-h4 pt-4 pl-10">Войти в магазин</v-card-title>
+    <v-card-title class="text-h4 pt-4 pl-10">Вход</v-card-title>
+    <v-card-content>
+      <v-form
+          ref="form"
+          v-model="valid"
+          class="pl-5 pr-5"
+          lazy-validation
+      >
+        <v-text-field
+            v-model="name"
+            :rules="nameRules"
+            label="Имя"
+            required
+        ></v-text-field>
+
+        <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+        ></v-text-field>
+
+        <v-btn block
+            :disabled="!valid"
+            color="#1DE9B6"
+            @click="validate"
+        >
+          Войти
+        </v-btn>
+
+
+      </v-form>
+    </v-card-content>
   </v-card>
 </template>
 
-<script>
+<script setup>
+const form = ref();
+let valid = true;
+let name = '';
+const nameRules = [
+  v => !!v || 'Это поле нужно заполнить',
+];
+const email = '';
+const emailRules = [
+  v => !!v || 'Это поле нужно заполнить',
+  v => /.+@.+\..+/.test(v) || 'Введите валидный E-mail',
+];
+
+const validate = () => {
+  form.value.validate();
+};
 </script>
 
 <style scoped>
 .form {
   background-color: white;
-  min-height: 500px;
+  min-height: 300px;
   z-index: 10000;
 }
 </style>
